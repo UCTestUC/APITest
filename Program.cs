187f76c70766b6cfc29116672bca8a1cf0a5cddb
UCTestUC <cathyzhang2018@gmail.com>
2022-08-06 19:24:31 +1000

    add swagger

--- Program.cs
+++ Program.cs
@@ -5,7 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen((c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                { Title = "My API", 
+                Version = "v1" });
+            });
+
+
 builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.Configure<Location>(builder.Configuration.GetSection("Location"));
@@ -14,11 +21,16 @@ builder.Services.Configure<Location>(builder.Configuration.GetSection("Location"
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+/*
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+*/
+
+pp.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.UseHttpsRedirection();
